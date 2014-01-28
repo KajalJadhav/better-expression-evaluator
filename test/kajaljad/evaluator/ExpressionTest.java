688be6d2d9f5ac249ec3372e2f045d819bb8db1e
Kajal <kajal@inankhome.com>
2014-01-28 14:36:09 +0530

    Tested for multiple Expression

--- test/kajaljad/evaluator/ExpressionTest.java
+++ test/kajaljad/evaluator/ExpressionTest.java
@@ -34,4 +34,52 @@ public class ExpressionTest {
         Expression expression = new Expression(new Expression(10d), new Expression(2d), new Exponent());
         assertEquals(100d, expression.evaluate());
     }
+
+    @Test
+    public void forEvaluatingExpression() throws Exception {
+        Expression twoPlusThree = e(e(3), e(2), plus());
+        Expression multiplyByFour = e(twoPlusThree, e(4), multiply());
+        Expression threeMultiplyByTwo = e(e(3), e(2), multiply());
+        Expression expression = e(multiplyByFour, threeMultiplyByTwo, plus());
+        assertEquals(26.0, expression.evaluate());
+    }
+
+    @Test
+    public void forEvaluatingExpressionWithBrackets() throws Exception {
+        Expression twoPlusFour = e(e(4), e(2), plus());
+        Expression divideByThree = e(twoPlusFour, e(3), divide());
+        Expression expression = e(twoPlusFour, divideByThree, plus());
+        assertEquals(6.5, expression.evaluate());
+    }
+
+    @Test
+    public void forEvaluatingExpressionWithExponent() throws Exception {
+        Expression twoPlusThree = e(e(2), e(3), plus());
+        Expression expression = e(e(2),twoPlusThree, exponent());
+        assertEquals(25d, expression.evaluate());
+    }
+
+    public static Expression e(double n) {
+        return new Expression(n);
+    }
+
+    public static Expression e(Expression l, Expression r, Operator o) {
+        return new Expression(r, l, o);
+    }
+
+    public static Operator plus() {
+        return new Addition();
+    }
+
+    public static Operator multiply() {
+        return new Multiplication();
+    }
+
+    public static Operator divide() {
+        return new Division();
+    }
+
+    public static Operator exponent() {
+        return new Exponent();
+    }
 }
